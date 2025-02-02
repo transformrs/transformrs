82b2415c1e4b3d298a734fe6a5943eada109285c
Rik Huijzer <github@huijzer.xyz>
2025-02-02 11:04:38 +0100

    Test stream duration

--- tests/openai.rs
+++ tests/openai.rs
@@ -6,6 +6,8 @@ use aiapi::Provider;
 use futures_util::stream::StreamExt;
 use serde_json::Value;
 
+const MODEL: &str = "meta-llama/Llama-3.3-70B-Instruct-Turbo";
+
 #[tokio::test]
 async fn test_chat_completion() {
     let messages = vec![
@@ -20,8 +22,7 @@ async fn test_chat_completion() {
     ];
     let key = aiapi::read_key();
     let provider = Provider::DeepInfra;
-    let model = "meta-llama/Llama-3.3-70B-Instruct-Turbo";
-    let resp = openai::chat_completion(&key, &provider, model, false, &messages).await;
+    let resp = openai::chat_completion(&key, &provider, MODEL, false, &messages).await;
     let resp = resp.unwrap();
     let json = resp.json::<Value>().await.unwrap();
     let content = openai::chat_completion_content(json).await;
@@ -43,8 +44,7 @@ async fn test_chat_completion_stream() {
     ];
     let key = aiapi::read_key();
     let provider = Provider::DeepInfra;
-    let model = "meta-llama/Llama-3.3-70B-Instruct-Turbo";
-    let resp = openai::chat_completion(&key, &provider, model, true, &messages).await;
+    let resp = openai::chat_completion(&key, &provider, MODEL, true, &messages).await;
     let resp = resp.unwrap();
     let mut stream = openai::chat_completion_stream(resp).await.unwrap();
     let mut content = String::new();
@@ -55,4 +55,44 @@ async fn test_chat_completion_stream() {
         content += &chunk;
     }
     assert_eq!(content, "hello world");
+}
+
+#[tokio::test]
+async fn test_chat_completion_stream_duration() {
+    let messages = vec![
+        Message {
+            role: "system".to_string(),
+            content: "You are a helpful assistant.".to_string(),
+        },
+        Message {
+            role: "user".to_string(),
+            content: "Tell a joke about a car.".to_string(),
+        },
+    ];
+    let key = aiapi::read_key();
+    let provider = Provider::DeepInfra;
+    let resp = openai::chat_completion(&key, &provider, MODEL, true, &messages).await;
+    let resp = resp.unwrap();
+    let mut stream = openai::chat_completion_stream(resp).await.unwrap();
+    let mut content = String::new();
+    let mut timestamps = Vec::new();
+    while let Some(json) = stream.next().await {
+        let timestamp = std::time::SystemTime::now();
+        let chunk = openai::chat_completion_stream_content(json.unwrap())
+            .await
+            .unwrap();
+        content += &chunk;
+        println!("{}", chunk);
+        timestamps.push(timestamp);
+    }
+    let first_timestamp = timestamps.first().unwrap();
+    let last_timestamp = timestamps.last().unwrap();
+    // The car joke is probably one or two sentences, which should take at least some ms.
+    let expected_duration = std::time::Duration::from_millis(100);
+    let total_duration = last_timestamp.duration_since(*first_timestamp).unwrap();
+    assert!(total_duration >= expected_duration,
+        "Streaming response took {} ms, expected at least {} ms",
+        total_duration.as_millis(),
+        expected_duration.as_millis()
+    );
 }
