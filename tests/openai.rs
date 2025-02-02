f135a335d62b71c691a36603dfdbe62b623eccbd
Rik Huijzer <github@huijzer.xyz>
2025-02-02 15:34:16 +0100

    Test OpenAI too

--- tests/openai.rs
+++ tests/openai.rs
@@ -9,6 +9,10 @@ const MODEL: &str = "meta-llama/Llama-3.3-70B-Instruct-Turbo";
 
 #[tokio::test]
 async fn test_chat_completion_no_stream() {
+    let providers = vec![
+        (Provider::DeepInfra, MODEL),
+        (Provider::OpenAI, "gpt-4o-mini"),
+    ];
     let messages = vec![
         Message {
             role: "system".to_string(),
@@ -20,14 +24,16 @@ async fn test_chat_completion_no_stream() {
         },
     ];
     let keys = aiapi::read_keys();
-    let key = keys.for_provider(&Provider::DeepInfra).unwrap();
-    let resp = openai::chat_completion(&key, &MODEL, &messages)
-        .await
-        .unwrap();
-    println!("{:?}", resp);
-    assert_eq!(resp.object, "chat.completion");
-    assert_eq!(resp.choices.len(), 1);
-    assert_eq!(resp.choices[0].message.content, "hello world");
+    for (provider, model) in providers {
+        let key = keys.for_provider(&provider).unwrap();
+        let resp = openai::chat_completion(&key, model, &messages)
+            .await
+            .unwrap();
+        println!("{:?}", resp);
+        assert_eq!(resp.object, "chat.completion");
+        assert_eq!(resp.choices.len(), 1);
+        assert_eq!(resp.choices[0].message.content, "hello world");
+    }
 }
 
 #[tokio::test]
